865c70e39c51c4c6e8556966c64693636d4a3a37
Radhika <rsonde123@gmail.com>
2023-02-18 20:08:14 -0600

    pages framework except login & client registration

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// const express = require("express");
-// const path = require('path')
-
-// let index = new express();
-// const publicPath = path.join(__dirname,'public')
-
-// //GET, POST, ...
-// index.get("/",function(req, res) {
-//     res.send("Hello from Node Sample");
-// });
-
-// index.get("/about",function(req, res){
-//     res.send("About web page");
-// });
-
-// index.get("/contact",function(req,res){
-//     res.send("@rsond");
-// });
-
-// index.use(express.static(publicPath))
-
-// index.get("*",function(req,res){
-//     res.send("");
-// });
-
-
-// let port = 12345;
-// index.listen(port,function(){
-//     console.log("Server started listening at localhost:"+port);
-// });
-// server.js
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -44,6 +13,11 @@ app.get('/', (req, res) => {
 // Define a route for the fuel page
 app.get('/fuel', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'fuel.html'));
+});
+
+// Define a route for the fuel quote history page
+app.get('/fuelhistory', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'fuelhistory.html'));
 });
 
 // Start the server
