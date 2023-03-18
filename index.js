9ac06e10148774301ab7c498686ccea12e6c2cc1
Radhika <rsonde123@gmail.com>
2023-03-17 22:48:31 -0500

    backend frontend changes

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// const express = require('express');
-// const app = express();
-// const path = require('path');
-
 const express = require("express")
 const path = require('path');
 const collection = require("./mongo")
 const cors = require("cors")
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
 
 // Set up middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 // Define a route for the login page
 app.get('/', (req, res) => {
@@ -31,10 +31,37 @@ app.get('/fuel', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'fuel.html'));
 });
 
-// Define a route for the fuel quote history page
-app.get('/fuelhistory', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'fuelhistory.html'));
+//pulls information from front end fuel page
+app.post("/fuel", function(req,res){
+  let fuel = {
+      userid: "1",
+      gallons: req.body.gallons,
+      date: req.body.date,
+      price: req.body.price,
+      total: req.body.total
+  };
+
+  console.log(fuel);
+  res.redirect("/fuel");
 });
+
+
+// Define a route for the fuel quote history page
+let fuelhistory = [
+  {userid: 1, gallons: 10, price: 5, total: 50 },
+  {userid: 1, gallons: 20, price: 5, total: 100 },
+  {userid: 1, gallons: 30, price: 7, total: 210 },
+  {userid: 2, gallons: 40, price: 7, total: 280 },
+  {userid: 1, gallons: 30, price: 20, total: 500 }
+];
+//uses ejs file instead of static html to display fuel history
+app.get('/fuelhistory', (req, res) => {
+  res.render('fuelhistory', {fuelList: fuelhistory})
+});
+// app.get('/fuelhistory', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'fuelhistory.html'));
+// });
+
 
 // Define a route for the Registration page
 app.get('/clientRegistration', (req, res) => {
@@ -42,6 +69,7 @@ app.get('/clientRegistration', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server started on port localhost:3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
 });
