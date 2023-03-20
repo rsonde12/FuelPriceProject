aaca92e2c0e901233f576910029b05905f6f0099
leah <leahljoseph@gmail.com>
2023-03-20 15:21:27 -0500

    chnage

--- mongoClient.js
+++ mongoClient.js
@@ -123,3 +123,73 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+
+
+
+// File Description: This is the main file for our server stuff and backend. The route handling (GET, POST, DELETE) and all other updates are in the route folder.
+// const mongoose = require('mongoose');
+
+// // Make the express app listen to server:
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(() => {
+//         // After connected to DB, then listen to express application:
+//         app.listen(processs.env.PORT, () => {
+//             console.log("Conneted to the database and listening on port 3000!");
+//         });
+//     })
+//     .catch((error) => {
+//         // Log any errors to console:
+//         console.log(error);
+//     });
+
+// const moongoose = require("mongoose")
+// moongoose.connect("mongodb://0.0.0.0:27017/testing")
+// .then(() =>{
+//     console.log("mongodb connected");
+// })
+// .catch(() =>{
+//     console.log('failed');
+// })
+
+// const profileSchema=new moongoose.Schema({
+//     name:{
+//         type:String,
+//         required:true
+//     },
+//     address:{
+//         type:String,
+//         required:true
+//     },
+//     address2:{
+//         type:String,
+//     },
+//     city:{
+//         type:String,
+//         required:true
+//     },
+//     state:{
+//         type:String,
+//         required:true
+//     },
+//     zipcode:{
+//         type:Number,
+//     }
+// })
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/testing";
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mydb");
+//   var myData = {'name':'b', 'price':10, 'inStock':false};
+//   dbo.collection("smartphones").insertOne(myData, function(err, res) {
+//     if (err) throw err;
+//     console.log("1 document inserted");
+//     db.close();
+//   });
+// });
+
+
+// module.exports = moongoose.model("fuel", newSchema)
