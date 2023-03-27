7c3ccd527d4161b1b3bc2fd56755bcd40bccd54b
Jvetticadan <81869060+Jvetticadan@users.noreply.github.com>
2023-03-26 22:36:30 -0500

    Update login.js

--- models/login.js
+++ models/login.js
@@ -2,25 +2,30 @@ var attempt = 3;
 
 function validate()
 {
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
+var username = document.getElementById("Username").value;
+var password = document.getElementById("Password").value;
 
-if ( username == "Formget" && password == "formget#123")
+if ( username == "admin123" && password == "lamba3")
 {
-  alert ("Login successfully");
-  window.location = "success.html"; // Redirecting to other page.
+  alert ("You have successfully accessed your account!");
+  window.location = "clientprofile.html"; 
   return false;
 }
 else
 {
-  attempt --;// Decrementing by one.
-  alert("You have left "+attempt+" attempt;");
+  attempt --;
+  alert("Wrong account information, you have only "+attempt+" left.");
 
 if( attempt == 0)
     {
-      document.getElementById("username").disabled = true;
-      document.getElementById("password").disabled = true;
-      document.getElementById("submit").disabled = true;
+      document.getElementById("Username").disabled = true;
+      //changes the by true 0 and False 1
+      document.getElementById("Password").disabled = true;
+      
+      
+      
+      
+      document.getElementById("Success").disabled = true;
       return false;
     }
   }
