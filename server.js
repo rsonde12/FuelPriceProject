fcf030a67bd9158f91560aad846b6141891cdcad
leah <leahljoseph@gmail.com>
2023-03-31 10:05:35 -0500

    delete

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const collection = express("./mongo");
-
-const cors = require("cors")
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
