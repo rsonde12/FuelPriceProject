611cd067ca2b08c23d07649d01af7fc0d33bef33
leah <leahljoseph@gmail.com>
2023-03-31 10:09:57 -0500

    change

---
+++ nextConfig.js
@@ -0,0 +1,12 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     swcMinify: true,
+  
+//     env: {
+//       MONGODB_URI: "mongodb+srv://admin:reservationsystem@cluster0.k34jeu6.mongodb.net/?retryWrites=true&w=majority"
+//     }
+//   }
+  
+//   module.exports = nextConfig
+  
